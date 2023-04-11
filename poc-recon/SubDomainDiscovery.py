e1a30ee779a6b6104375b5931f94e7e82e4709a3
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-04-11 17:48:37 +0200

    test shodan scan

--- poc-recon/SubDomainDiscovery.py
+++ poc-recon/SubDomainDiscovery.py
@@ -6,10 +6,11 @@ import requests
 import sys
 import subprocess
 import socket
+import json
 
-domain =    sys.argv[1]
-mode =      sys.argv[2]
-#output =    sys.argv[3]
+domain = sys.argv[1]
+mode = sys.argv[2]
+shodan_key = sys.argv[3]
 
 ### function def
 def tabber(string):
@@ -25,15 +26,31 @@ if "amass" in mode:   # search subdomain via DuckDuckGo
     subdomains = result.splitlines()
 
     for subdomain in subdomains:
+
+        # gat domain info
         try:
 
             subdomain_ip = socket.gethostbyname(subdomain)
             subdomain_str = subdomain.decode("utf-8")
             tab = tabber(subdomain_str)
-            print("| {} {} | {}\t|".format(subdomain_str, tab, subdomain_ip))
+            # print("| {} {} | {}\t|".format(subdomain_str, tab, subdomain_ip))
+            output = "| {} {} | {}\t|".format(subdomain_str, tab, subdomain_ip)
 
         except:
 
             subdomain_str = subdomain.decode("utf-8")
             tab = tabber(subdomain_str)
-            print("| {} {} | n/a \t\t|".format(subdomain_str, tab))
+            # print("| {} {} | n/a \t\t|".format(subdomain_str, tab))
+            output = "| {} {} | n/a \t\t|".format(subdomain_str, tab)
+        
+        # get shodan info
+        try:
+
+            shodan_info = requests.get("https://api.shodan.io/shodan/host/{}?key={}".format(subdomain_ip, shodan_key))
+            shodan_json = json.loads(shodan_info)
+            ports = shodan_json["ports"]
+            output = output + " {} |".format(ports)
+
+        except:
+
+            output = output + " n/a | "
