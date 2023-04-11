a560ef7af66a5004ba80408f6ab4d67d51e0cf71
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-04-11 15:56:25 +0200

    add info

--- poc-recon/SubDomainDiscovery.py
+++ poc-recon/SubDomainDiscovery.py
@@ -5,11 +5,19 @@ Usege:  SubDomainDiscovery --domain contoso.hack --mode amass
 import requests
 import sys
 import subprocess
+import socket
 
 domain =    sys.argv[1]
 mode =      sys.argv[2]
 #output =    sys.argv[3]
 
+print
+
 if "amass" in mode:   # search subdomain via DuckDuckGo
     result = subprocess.check_output(["amass", "enum", "-passive", "-d", domain])
-    print(result.decode("utf-8"))
+    #print(result.decode("utf-8"))
+    subdomains = result.splitlines()
+
+    for subdomain in subdomains:
+        subdomain_info = socket.getaddrinfo(subdomain, None)
+        print(subdomain_info)
