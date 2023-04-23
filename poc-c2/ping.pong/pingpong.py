e6a75961af82c0ef3312ff88c8c75d3b362b3549
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-04-23 10:07:30 +0200

    fix pkt seen

--- poc-c2/ping.pong/pingpong.py
+++ poc-c2/ping.pong/pingpong.py
@@ -14,9 +14,9 @@ def read(packet):
             print(data)
         elif source_os == 'win':
             data = packet[ICMP].load
-            if data not in packets_seen:
-                packets_seen.add(data)
-                print("Size: {}".format(len(data)))
+            #if data not in packets_seen:
+            packets_seen.add(data)
+            print("Size: {}".format(len(data)))
     else:
         print("No ICMP packets")
 
