0578db0f935007c75b7fb4c1541a264620e1932a
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-04-23 23:08:22 +0200

    reader

---
+++ poc-c2/ping.pong/readpong.py
@@ -0,0 +1,16 @@
+
+import time
+import os
+import platform
+
+last_status_t = int(time.time())
+val = True
+
+while val:
+    file_status = os.stat("./stream.txt")
+    file_status_t = file_status.st_ctime
+    if file_status_t > last_status_t:
+        print("file change")
+        last_status_t = file_status_t
+    else:
+        print("...")
