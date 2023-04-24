d312c349a7a198d9f1f58a3339e334c19c9ae516
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-04-24 10:20:30 +0200

    test 3: polling

--- poc-c2/ping.pong/readpong.py
+++ poc-c2/ping.pong/readpong.py
@@ -6,9 +6,10 @@ import subprocess
 import select
 
 # var
-filename = "/home/sheliak/Labz/my-papers/poc-c2/ping.pong/stream.txt"
+file = "/home/sheliak/Labz/my-papers/poc-c2/ping.pong/stream.txt"
 
 '''
+# Test 1
 last_status_t = int(time.time())
 val = True
 
@@ -20,8 +21,8 @@ while val:
         last_status_t = file_status_t
     else:
         print(file_status.st_ctime, last_status_t)
-'''
 
+# Test 2
 tailprocess = subprocess.Popen(["tail", "-f", filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 while True:
@@ -30,3 +31,18 @@ while True:
         time.sleep(5)
         print("...")
     print(line.rstrip())
+'''
+
+tailp = os.popen("tail -f {}".format(file))     # tail
+
+output = select.poll()                          # read tail output
+output.register(tailp.fileno(), select.POLLIN)  # read tail output
+
+while True:
+    if output.poll(1000):
+        line = tailp.readline().strip()
+        if line:
+            print(line)
+    else:
+        print("...")
+
