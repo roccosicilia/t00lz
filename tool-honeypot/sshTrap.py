7806f33c8d289d282d94cede9a79f34a7a1d6343
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-05-17 11:29:04 +0200

    change struct

--- tool-honeypot/sshTrap.py
+++ tool-honeypot/sshTrap.py
@@ -4,24 +4,28 @@ import os
 dir = os.path.dirname(os.path.abspath(__name__))
 server_ip = '172.25.82.136'  ## use your server ip
 
-def simulate_ssh(addr):
-    username = input("Username: ")
-    password = input("Password: ")
+def process_data(data):
+    if data == "5":
+        return "grazie"
+    else:
+        return "Dato non valido"
 
-    with open("{}/ssh_log.txt".format(dir), "a") as log_file:
-        log_file.write(f"IP: {addr[0]}:{addr[1]}, Username: {username}, Password: {password}\n")
-        log_file.write("Invalid credentials\n")
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, 2222))
+    server_socket.listen(1)
+    print("In attesa di connessioni...")
 
-    print("Invalid credentials")
+    while True:
+        conn, addr = server_socket.accept()
+        print(f"Connessione accettata da {addr[0]}:{addr[1]}")
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_ip, 2222))
-server_socket.listen(1)
+        data = conn.recv(1024).decode()
+        print(f"Dato ricevuto: {data}")
 
-print("Listening... ")
+        response = process_data(data)
+        conn.sendall(response.encode())
 
-while True:
-    conn, addr = server_socket.accept()
-    print(f"Connessione accettata da {addr[0]}:{addr[1]}")
-    simulate_ssh(addr)
-    conn.close()
+        conn.close()
+
+start_server()
